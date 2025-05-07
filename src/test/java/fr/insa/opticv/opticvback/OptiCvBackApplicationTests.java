07f6dde6ec4f0afc3cedbace885652727592a9eb
danielhabib3 <danielhabib301@gmail.com>
2025-05-07 11:33:42 +0200

    initial commit

---
+++ src/test/java/fr/insa/opticv/opticvback/OptiCvBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.opticv.opticvback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiCvBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
